5ac721776e12c23d988c3fd9ecfb6d0097fb55a9
Jacek <xjack13@wp.pl>
2019-05-02 22:42:58 +0200

    Add integer calculator functions

--- CalculatorJUnit/src/calculatorjunit/CalculatorJUnit.java
+++ CalculatorJUnit/src/calculatorjunit/CalculatorJUnit.java
@@ -11,11 +11,39 @@ package calculatorjunit;
  */
 public class CalculatorJUnit {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public int addNumber(int a, int b){
+        return a+b;
     }
-    
+
+    public int subtractNumber (int a, int b){
+        return a-b;
+    }
+
+    public int multiplyNumber (int a, int b){
+        return a*b;
+    }
+
+    public static int divideNumber(int a, int b){
+        if (b == 0) {
+            throw new IllegalArgumentException("Niedozwolone dzielenie przez 0");
+        }
+        return a/b;
+    }
+
+    public static long factorialNumber(int number) {
+        int result = 1;
+
+        for (int factor = 2; factor <= number; factor++) {
+            result *= factor;
+        }
+
+        if (number<0) {
+            throw new IllegalArgumentException("Niedozwolony parametr mniejszy od 0");
+        }
+        System.out.println("wynik silni =" + result);
+        return result;
+    }
+
+    public static void main(String[] Args) {
+        }
 }
