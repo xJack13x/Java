e6ce4665d756327c9733bac84bd76e5b7efabe17
Jacek <xjack13@wp.pl>
2019-05-10 18:00:42 +0200

    Add first change JavaFx

--- IsJavaFX/src/isjavafx/IsJavaFX.java
+++ IsJavaFX/src/isjavafx/IsJavaFX.java
@@ -22,7 +22,7 @@ public class IsJavaFX extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("'Hello World'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
