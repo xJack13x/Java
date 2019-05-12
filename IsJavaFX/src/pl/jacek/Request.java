e994964e701f24943d1c963377368b8cfef7d59d
Jacek <xjack13@wp.pl>
2019-05-12 21:43:49 +0200

    Add main window

--- IsJavaFX/src/pl/jacek/Request.java
+++ IsJavaFX/src/pl/jacek/Request.java
@@ -1,7 +1,14 @@
 package pl.jacek;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -22,5 +29,15 @@ public class Request {
     public void open() {
         Scene scene = new Scene(this.borderPane);
         this.stage = new Stage();
+        stage.setScene(scene);
+        stage.setResizable(true);
+        stage.setFullScreen(false);
+        stage.setTitle("Integracja systemów");
+        stage.show();
     }
+   
+
+    
+    
+    
 }
